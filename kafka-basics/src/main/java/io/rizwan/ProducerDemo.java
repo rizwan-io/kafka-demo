b985a57a7713f3ac261090f725bfe2c821249fe8
rizwan <rizwan.sha993@gmail.com>
2024-01-20 12:42:45 +0530

    kafka producer demo

---
+++ kafka-basics/src/main/java/io/rizwan/ProducerDemo.java
@@ -0,0 +1,40 @@
+package io.rizwan;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger logger = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        logger.info("I am Kafka Producer");
+
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "Hello World");
+
+        // send data - this is asynchronous operation
+        producer.send(producerRecord);
+
+        // flush and close the producer - flush is sync operation
+        producer.flush();
+
+        // close automatically calls flush no not required to explicitly call flush
+        producer.close();
+    }
+}
